87e2c44c0a4b5404bf263a9c2bed338e5374eca5
Karem1986 <karem.ortiz@outlook.com>
2020-06-27 12:36:32 +0200

    Pokemon.js

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,12 +4,13 @@ export default function Pokemon() {
   return (
     <div>
       <h2>Apps built with React:</h2>
-      <ul>
-        <li>Instagram</li>
-        <li>Twitter</li>
-        <li>Snapchat</li>
-        <li>Redddit</li>
-      </ul>
+
+      <p>Instagram</p>
+      <p>Twitter</p>
+      <p>Snapchat</p>
+
+      <p>This can be equat to: {["applications", "mobileapps", "more"]}</p>
+
       <p>Awesoness is an understatement using React!</p>
     </div>
   );
