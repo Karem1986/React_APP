edca9737b5dd9b76b2d0f5b699ae04004194fb02
Karem1986 <karem.ortiz@outlook.com>
2020-06-27 12:04:10 +0200

    Added Pokemon.js

---
+++ src/components/Pokemon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Pokemon() {
+  return (
+    <div>
+      <h2>Apps built with React:</h2>
+      <ul>
+        <li>Instagram</li>
+        <li>Twitter</li>
+        <li>Snapchat</li>
+        <li>Redddit</li>
+      </ul>
+      <p>Awesoness is an understatement using React!</p>
+    </div>
+  );
+}
