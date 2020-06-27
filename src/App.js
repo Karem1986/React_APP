d18cc87fe03543728ecbcd6d7fd4d23a244befd0
Karem1986 <karem.ortiz@outlook.com>
2020-06-27 08:57:54 +0200

    uploaded react app first version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>What can I build with React?</code> looking forward 
+         to building
+          an application with it! 
         </p>
         <a
           className="App-link"
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello React!
         </a>
       </header>
     </div>
