ff0dbff14012bf14048d89d916663dc52db76cb2
Karem1986 <karem.ortiz@outlook.com>
2020-06-27 10:45:56 +0200

    fixed issue with Title.js

--- src/Title.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Title() {
-  return <h1>Hi there! </h1>;
-}
-
-Title();
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Title() {
+  return <h1>Hi there!</h1>;
+}
